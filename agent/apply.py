3f7de28c8879eba861ba5471e968c332eca7ec20
Vidyuth-Ramkumar <vidyuth@objectways.com>
2026-05-22 17:08:27 -0700

    playwright fixes

--- agent/apply.py
+++ agent/apply.py
@@ -9,20 +9,20 @@ load_dotenv()
 phone_number = os.getenv('PHONE_NUMBER')
 resume_file_path = os.getenv('RESUME_FILE_PATH')
 
-async def apply_to_single_job(job):
+def apply_to_single_job(job):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(job['url'])
-        await page.get_by_role('button', { 'name': 'Apply' }).click()
-        await page.getByLabel('First Name').fill(os.getenv('CANDIDATE_FIRST_NAME'))
-        await page.getByLabel('Last Name').fill(os.getenv('CANDIDATE_LAST_NAME'))
-        await page.getByLabel('Email').fill(os.getenv('CANDIDATE_EMAIL'))
+        page.get_by_role("button", name="Apply").click()
+        page.get_by_label('First Name').fill(os.getenv('CANDIDATE_FIRST_NAME'))
+        page.get_by_label('Last Name').fill(os.getenv('CANDIDATE_LAST_NAME'))
+        page.get_by_label('Email').fill(os.getenv('CANDIDATE_EMAIL'))
         # await page.getByLabel('Country').fill('')
-        await page.getByLabel('Phone').fill(os.getenv('CANDIDATE_PHONE_NUMBER'))
-        await page.getByLabel('Linkedin').fill(os.getenv('CANDIDATE_PHONE_NUMBER'))
-        await page.getByLabel('Resume/CV').setInputFiles(resume_file_path)
-        await page.get_by_role('button', { 'name': 'Submit' }).click()
+        page.get_by_label('Phone').fill(os.getenv('CANDIDATE_PHONE_NUMBER'))
+        page.get_by_label('Linkedin').fill(os.getenv('CANDIDATE_PHONE_NUMBER'))
+        page.get_by_label('Resume/CV').set_input_files(resume_file_path)
+        page.get_by_role("button", name="Submit").click()
 
 
 
@@ -45,7 +45,7 @@ apply_tool_schema = {
             "description": "The Greenhouse job posting url that you need to access to apply to the job"
         }
       },
-      "required": ["expression"]
+      "required": ["title", "url"]
     }
   }
 }
