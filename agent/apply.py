739bf5dd97d747c26df7f42e1b2ff5d12345d9c0
Vidyuth-Ramkumar <vidyuth@objectways.com>
2026-06-03 12:55:05 -0700

    successfully collected unknown fields

--- agent/apply.py
+++ agent/apply.py
@@ -23,7 +23,16 @@ POSSIBLE_FORM_FIELDS = {
     "city": [
         "City",
         "city",
+        "Location (City)*",
+        "Location (City)",
+        "Location*",
+        "Location",
+        "location*",
+        "location",
+        "Candidate-location",
+        "candidate-location",
     ],
+    # "location": ["Location", "location", "Candidate-location", "candidate-location"],
     "state": [
         "State",
         "state",
@@ -37,7 +46,7 @@ POSSIBLE_FORM_FIELDS = {
         "LinkedIn URL",
         "Linkedin URL",
     ],
-    "resume": ["Resume/CV", "Resume/CV*", "Resume", "CV", "Upload Resume"],
+    "resume": ["Resume/CV", "Resume/CV*", "Resume", "CV", "Upload Resume", "resume"],
     "authorized_to_work": [],
 }
 
@@ -181,12 +190,35 @@ def better_fill_field(
                 except Exception as e:
                     print(f"❌ Could not fill COUNTRY field; exception {e}")
                 return True
+            elif possible_field == "city":
+                try:
+                    print(
+                        f"Interacting with CITY field. field_id: {field_id}, possible_field from field variation map: {possible_field}"
+                    )
+                    browser_page.locator("#candidate-location").click(
+                        timeout=3000
+                    )  # Click open the search dropdown
+                    browser_page.keyboard.type(
+                        "Phoenix, Arizona, United States"
+                    )  # Type into it to search
+                    browser_page.get_by_text(
+                        "Phoenix, Arizona, United States", exact=True
+                    ).first.click(
+                        timeout=3000
+                    )  # click the correct one by exact text
+                    print("✓ Filled city")
+                except Exception as e:
+                    print(f"❌ Could not fill CITY field; exception {e}")
             else:
                 try:
                     print(
-                        f"Interacting with field_id: {field_id}; possible_field field variation map: {possible_field}"
+                        f"Interacting with field_id: {field_id}; possible_field: {possible_field}"
                     )
-                    field_locator.fill(FIELD_KEYS_TO_ENTRIES[possible_field])
+                    value = FIELD_KEYS_TO_ENTRIES[possible_field]
+                    if isinstance(value, list):
+                        try_select_or_fill(field_locator, value)
+                    else:
+                        field_locator.fill(value)
                     print(f"✓ Filled {possible_field}")
                     return True
                 except Exception as e:
@@ -210,9 +242,12 @@ def apply_to_single_job(job):
         known_fields, unknown_fields = [], []
         # existing_field_statuses = {"In Known Fields": False, "Filled": False}
         for ejf_locator in existing_job_field_locators:
-            field_tag_name = ejf_locator.evaluate(
-                "element => element.tagName.toLowerCase()"
-            )
+            try:
+                field_tag_name = ejf_locator.evaluate(
+                    "element => element.tagName.toLowerCase()"
+                )
+            except:
+                continue
             field_id = ejf_locator.get_attribute("id") or "Unnamed Field"
             field_aria_raw = (
                 ejf_locator.get_attribute("aria-label")
@@ -225,7 +260,7 @@ def apply_to_single_job(job):
                 field_aria = browser_page.locator(f"#{field_aria_raw}").text_content()
             else:
                 field_aria = field_aria_raw
-
+            print(f"Resolved field_aria: {field_aria}")
             if field_tag_name == "textarea":
                 was_filled = better_fill_field(
                     browser_page, ejf_locator, field_tag_name, field_id, field_aria
@@ -236,6 +271,10 @@ def apply_to_single_job(job):
                     unknown_fields.append(
                         {
                             "Field ID": field_id,
+                            "field_type": ejf_locator.get_attribute("type")
+                            or "textarea/select",
+                            "Field Aria": field_aria,
+                            "Field Tag": field_tag_name,
                         }
                     )
             if field_tag_name == "input":
@@ -249,16 +288,8 @@ def apply_to_single_job(job):
                         known_fields.append(field_id)
                     else:
                         unknown_fields.append(field_id)
-                # elif input_type in ["checkbox", "radio"]:
+                # if input_type in ["checkbox", "radio"]:
                 #     ejf_locator.check()
-            ejf_json = {
-                "field_id": ejf_locator.get_attribute("id") or "Unnamed Field",
-                "field_type": ejf_locator.get_attribute("type") or "textarea/select",
-                "field_aria ": ejf_locator.get_attribute("aria-label")
-                or ejf_locator.get_attribute("aria-labelledby")
-                or "No Aria",
-            }
-            print(ejf_json)
         # try:
         #     for possible_field in POSSIBLE_FORM_FIELDS:
         #         fill_field(browser_page, possible_field)
