73e15f2415f2f7e3d1c9af67651e7fff1ea3597f
Vidyuth-Ramkumar <vidyuth@objectways.com>
2026-06-02 21:10:34 -0700

    new filling function wip

--- agent/apply.py
+++ agent/apply.py
@@ -103,7 +103,6 @@ def try_select_or_fill(locator, values):
 # Tries all the possible field names
 # Country code dropdown HTML element is accessed by its id
 def fill_field(page, field_key):
-
     if field_key == "resume":
         try:
             page.locator("#resume").set_input_files(
@@ -114,7 +113,7 @@ def fill_field(page, field_key):
             print(f"✗ Could not find field: resume")
         time.sleep(1)
         return
-    if field_key == "country":
+    elif field_key == "country":
         try:
             page.locator("#country").click(timeout=3000)
             page.keyboard.type("United States")
@@ -141,59 +140,112 @@ def fill_field(page, field_key):
             print(f"✗ Could not find field: {field_key}")
 
 
-def better_fill_field(page, field_id):
-    return
-def apply_to_single_job(job):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        browser_page = browser.new_page()
-        browser_page.goto(job["url"])
-        browser_page.wait_for_load_state("networkidle")
+def better_fill_field(
+    browser_page, field_locator, field_tag_name, field_id, field_aria
+):
+    for possible_field in POSSIBLE_FORM_FIELDS:
+        if (
+            field_id in POSSIBLE_FORM_FIELDS[possible_field]
+            or field_aria in POSSIBLE_FORM_FIELDS[possible_field]
+        ):  # Found it in any of the possible fields' list of field name variations
+            if possible_field == "resume":
+                print(
+                    f"Interacting with RESUME field. field_id: {field_id}, possible_field field variation map: {possible_field}"
+                )
+                try:
+                    browser_page.locator("#resume").set_input_files(
+                        CANDIDATE_RESUME_FILE_PATH, timeout=3000
+                    )
+                    print(f"✓ Filled resume")
+                except Exception as e:
+                    print(f"❌ Could not fill RESUME field; exception {e}")
+                time.sleep(1)
+                return True
+            elif possible_field == "country":
+                try:
+                    print(
+                        f"Interacting with COUNTRY field. field_id: {field_id}, possible_field from field variation map: {possible_field}"
+                    )
+                    browser_page.locator("#country").click(
+                        timeout=3000
+                    )  # Click open the search dropdown
+                    browser_page.keyboard.type(
+                        "United States"
+                    )  # Type into it to search
+                    browser_page.get_by_text(
+                        "United States +1", exact=True
+                    ).first.click(
+                        timeout=3000
+                    )  # click the correct one by exact text
+                    print("✓ Filled country")
+                except Exception as e:
+                    print(f"❌ Could not fill COUNTRY field; exception {e}")
+                return True
+            else:
+                try:
+                    print(
+                        f"Interacting with field_id: {field_id}; possible_field field variation map: {possible_field}"
+                    )
+                    field_locator.fill(FIELD_KEYS_TO_ENTRIES[possible_field])
+                    print(f"✓ Filled {possible_field}")
+                    return True
+                except Exception as e:
+                    print(f"❌ Could not fill {possible_field.upper()} field; exception {e}")
+    return False    
 
-        fields_locator_filter = browser_page.locator(
-            "form input:not([type='submit']):not([type='button']), form select, form textarea"
-        )
-        existing_job_fields = fields_locator_filter.all()
-        known_fields, unknown_fields = [], []
-        # existing_field_statuses = {"In Known Fields": False, "Filled": False}
-        for ejf in existing_job_fields:
-            field_tag_name = ejf.evaluate("element => element.tagName.toLowerCase()")
-            field_id = ejf.get_attribute("id") or "Unnamed Field"
-            field_area = ejf.get_attribute("aria-label") or None
-            if field_tag_name == "textarea" or (
-                field_tag_name == "input"
-                and input_type in ["text", "email", "tel", "number"]
-            ):
-                better_fill_field(browser_page, field_id)
-            if field_tag_name == "input":
-                input_type = ejf.get_attribute("type") or "text"
-                input_type = input_type.lower()
-                if input_type in ["text", "email", "password", "tel", "number"]:
-                    ejf.fill("Sample Text")
-                elif input_type in ["checkbox", "radio"]:
-                    ejf.check()
-            ejf_json = {
-                "field_id": ejf.get_attribute("id") or "Unnamed Field",
-                "field_type": ejf.get_attribute("type") or "textarea/select",
-                "field_aria ": ejf.get_attribute("aria-label")
-                or ejf.get_attribute("aria-labelledby")
-                or "No Aria",
-            }
-            print(ejf_json)
-        # try:
-        #     for possible_field in POSSIBLE_FORM_FIELDS:
-        #         fill_field(browser_page, possible_field)
 
-        #     browser_page.get_by_role("button", name="Submit application").click(timeout=2000)
-        #     browser_page.wait_for_load_state("networkidle")
-        # except Exception as e:
-        #     print(f"Error filling form for {job['title']} at {job['company']}: \n{e}")
+    def apply_to_single_job(job):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
+            browser_page = browser.new_page()
+            browser_page.goto(job["url"])
+            browser_page.wait_for_load_state("networkidle")
 
-        if "confirmation" in browser_page.url:
-            print("SUCCESSFULLY SUBMITTED APPLICATION 🎉 🎊 🕺")
-        print(f"Final URL: {browser_page.url}")
-        input("Press Enter to close browser...")
-        browser_page.wait_for_load_state("networkidle")
+            fields_locator_filter = browser_page.locator(
+                "form input:not([type='submit']):not([type='button']), form select, form textarea"
+            )
+            existing_job_field_locators = fields_locator_filter.all()
+            known_fields, unknown_fields = [], []
+            # existing_field_statuses = {"In Known Fields": False, "Filled": False}
+            for ejf_locator in existing_job_field_locators:
+                field_tag_name = ejf_locator.evaluate(
+                    "element => element.tagName.toLowerCase()"
+                )
+                field_id = ejf_locator.get_attribute("id") or "Unnamed Field"
+                field_aria = ejf_locator.get_attribute("aria-label") or None
+                if field_tag_name == "textarea":
+                    was_filled = better_fill_field(
+                        browser_page, ejf_locator, field_tag_name, field_id, field_aria
+                    )
+                if field_tag_name == "input":
+                    input_type = ejf_locator.get_attribute("type") or "text"
+                    input_type = input_type.lower()
+                    if input_type in ["text", "email", "password", "tel", "number"]:
+                        ejf_locator.fill("Sample Text")
+                    elif input_type in ["checkbox", "radio"]:
+                        ejf_locator.check()
+                ejf_json = {
+                    "field_id": ejf_locator.get_attribute("id") or "Unnamed Field",
+                    "field_type": ejf_locator.get_attribute("type") or "textarea/select",
+                    "field_aria ": ejf_locator.get_attribute("aria-label")
+                    or ejf_locator.get_attribute("aria-labelledby")
+                    or "No Aria",
+                }
+                print(ejf_json)
+            # try:
+            #     for possible_field in POSSIBLE_FORM_FIELDS:
+            #         fill_field(browser_page, possible_field)
+
+            #     browser_page.get_by_role("button", name="Submit application").click(timeout=2000)
+            #     browser_page.wait_for_load_state("networkidle")
+            # except Exception as e:
+            #     print(f"Error filling form for {job['title']} at {job['company']}: \n{e}")
+
+            if "confirmation" in browser_page.url:
+                print("SUCCESSFULLY SUBMITTED APPLICATION 🎉 🎊 🕺")
+            print(f"Final URL: {browser_page.url}")
+            input("Press Enter to close browser...")
+            browser_page.wait_for_load_state("networkidle")
 
 
 apply_tool_schema = {
